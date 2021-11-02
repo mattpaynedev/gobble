0224d7c776a490f0f6a4c7fc3ef1dbef01a9c379
mattpaynedev <72278561+mattpaynedev@users.noreply.github.com>
2021-11-01 22:09:45 -0400

    Update home page and clean up

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,51 +1,75 @@
-import './Home.css'
+// import './Home.css'
 import Header from '../Header'
 import Footer from '../Footer'
+import React from 'react'
 import { Link } from 'react-router-dom'
+import HeaderComponent from '../Header';
+import { Box, Button, Heading, Main, Paragraph, Text } from 'grommet'
+import FooterComponent from '../Footer';
 
 export default function Home() {
 
     return (
-        <div className='body-wrapper'>
-            <div className='body-overlay'>
-                <div className='header-title-nav-wrapper'>
-                    <Header
-                        headerClass='home-header'
-                        navClass='home-nav'
-                    />
-                </div>
-                <main className='home-body-wrapper'>
-
-                    <div className='home-body-grid'>
-                        <div className='front-page-content-wrapper'>
-                            <div className='home-content-quote-wrapper'>
-                                <h3>I like on the table,</h3>
-                                <h3>when we're speaking,</h3>
-                                <h3>the light of a bottle</h3>
-                                <h3>of intelligent wine.</h3>
-                                <h4>-Pablo Neruda</h4>
-                            </div>
-
-                            <div className='home-content-down-arrow-link'>
-                                <a href='#site-description'>
-                                    <div className='home-content-down-arrow'>
-                                    </div>
-                                </a>
-                            </div>
-                        </div>
-                        <div id='site-description' className='home-content-wrapper'>
-                            <p>Gobble is a modern wine cellar management app.</p>
-                            <p>Use it to catalog and track your collection with simple, easy-to-use tools.</p>
-                            <Link to='/collections'><button
-                                className='home-content-btn'
-                            >Get Started</button></Link>
-                        </div>
-                    </div>
-                </main>
-                <Footer
-                    footerClass='home-footer'
-                />
-            </div>
-        </div >
+        <Box
+            background={
+                {
+                    color: "black",
+                    position: "center center",
+                    repeat: "no-repeat",
+                    size: "cover",
+                    image: "url(/background-compressed.jpg)"
+                }}
+            align="center"
+        >
+            <Box
+                background="#18181890"
+                width="100%"
+            >
+                <HeaderComponent />
+                <Main
+                    width="xxlarge"
+                    height={{ min: "100vh" }}
+                    pad="medium"
+                >
+                    <Box>
+                        <Box>
+                            <Box
+                                align="end"
+                                pad="medium"
+                                style={{
+                                    fontFamily: "'Josefin Slab', serif",
+                                    textShadow: "5px 5px 10px #181818"
+                                }}
+                            >
+                                <Heading level={1} margin="xsmall">I like on the table,</Heading>
+                                <Heading level={1} margin="xsmall">when we're speaking,</Heading>
+                                <Heading level={1} margin="xsmall">the light of a bottle</Heading>
+                                <Heading level={1} margin="xsmall">of intelligent wine.</Heading>
+                                <Heading level={3} margin="small">-Pablo Neruda</Heading>
+                            </Box>
+                        </Box>
+                        <Box
+                            direction="row-responsive"
+                            align="center"
+                            justify="evenly"
+                            margin={{ vertical: "medium" }}
+                        >
+                            <Box>
+                                <Paragraph margin="xsmall" weight="bold" size="xlarge">Gobble is a modern wine cellar management app.</Paragraph>
+                                <Paragraph margin="xsmall" weight="bold" size="xlarge">Use it to catalog and track your collection with simple, easy-to-use tools.</Paragraph>
+                            </Box>
+                            <Link to='/collections'>
+                                <Button
+                                    color="accent-1"
+                                    primary
+                                    hoverIndicator
+                                    label={<Text weight="bold" size="large">Get Started</Text>} />
+                            </Link>
+                        </Box>
+                    </Box>
+                </Main>
+                <FooterComponent />
+            </Box>
+        </Box>
     );
 }
