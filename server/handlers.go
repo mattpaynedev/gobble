535249d8fb51b959f82a8f9402e041105f8fe2a4
mattpaynedev <72278561+mattpaynedev@users.noreply.github.com>
2022-03-18 14:24:05 -0700

    Add blank handler function for Add Collection

--- server/handlers.go
+++ server/handlers.go
@@ -134,3 +134,35 @@ func (app *application) updateCollectionHandler(w http.ResponseWriter, r *http.R
 
 	json.NewEncoder(w).Encode(updatedCollection)
 }
+
+func (app *application) addCollectionHandler(w http.ResponseWriter, r *http.Request) {
+	// w.Header().Set("content-type", "application/json")
+
+	// vars := mux.Vars(r)
+	// collectionID, err := primitive.ObjectIDFromHex(vars["collect"])
+	// if err != nil {
+	// 	app.notFound(w)
+	// 	return
+	// }
+
+	// var newWine *models.Wines
+
+	// err = json.NewDecoder(r.Body).Decode(&newWine)
+	// if err != nil {
+	// 	app.serverError(w, err)
+	// 	return
+	// }
+
+	// newWine.ID = primitive.NewObjectID()
+
+	// insertedWine, err := app.wine.AddNewWine(newWine, collectionID)
+	// if err != nil {
+	// 	app.serverError(w, err)
+	// 	return
+	// }
+
+	// app.infoLog.Println("Wine inserted:", insertedWine.ID.String())
+
+	// json.NewEncoder(w).Encode(insertedWine)
+	app.infoLog.Println("new Collection")
+}
